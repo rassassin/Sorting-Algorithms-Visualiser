4640f8362095ef538130f3fcf60ad28562671903
Reece Thandi <reecethandi11@gmail.com>
2024-11-01 20:40:20 +0000

    created arraySupplier class

---
+++ arraySupplier.js
@@ -0,0 +1,24 @@
+const n = 100;
+
+class ArraySupplier {
+  constructor() {}
+  createOrderArray() {
+    const array = new Array(n).fill(0).map((_, i) => i + 1);
+    return array;
+  }
+
+  shuffleArray(orderedArray) {
+    for (let i = orderedArray.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let k = orderedArray[i];
+      orderedArray[i] = orderedArray[j];
+      orderedArray[j] = k;
+    }
+    return orderedArray;
+  }
+
+  getShuffledArray() {
+    const orderedArray = this.createOrderArray();
+    return this.shuffleArray(orderedArray);
+  }
+}
