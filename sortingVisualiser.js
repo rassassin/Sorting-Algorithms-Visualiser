5a03a5c5e24a8a5949effd075ea269d41601bd5d
Reece <reecethandi11@gmail.com>
2024-11-07 23:28:15 +0000

    refactored bubblesort function

--- sortingVisualiser.js
+++ sortingVisualiser.js
@@ -20,18 +20,19 @@ function draw() {
   const heightScale = window.innerHeight / Math.max(...listOfNums);
 
   for (let i = 0; i < listOfNums.length; i++) {
-    if (i < listOfNums.length) {
-      if (listOfNums[i] > listOfNums[i + 1]) {
-        swapValues(listOfNums, i, i + 1);
-      }
-    }
+    bubbleSort(listOfNums, i);
+
     const barHeight = listOfNums[i] * heightScale;
     rect(i * barWidth, window.innerHeight - 1 - barHeight, barWidth, barHeight);
   }
 }
 
-const swapValues = (arr, a, b) => {
-  let temp = arr[a];
-  arr[a] = arr[b];
-  arr[b] = temp;
+const bubbleSort = (arr, i) => {
+  if (i < listOfNums.length) {
+    if (listOfNums[i] > listOfNums[i + 1]) {
+      let temp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
+    }
+  }
 };
